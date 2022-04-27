a631f042e36552268fee63a26a365429e29da39f
Mathis <mathis.augereau@ynov.com>
2022-04-27 17:46:19 +0200

    'push'

--- Assets/Script/ItemVariables.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemVariable : MonoBehaviour{
-public string itemType;
-public string ItemID;
-public Sprite itemSprite;
-}
